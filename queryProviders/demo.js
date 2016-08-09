6936d269bde3b0106999a989fd2a6a16a7aa9ac7
Shirish Kamath <shirishk.87@gmail.com>
2016-08-09 22:51:48 +0530

    Comments explaining structure

--- queryProviders/demo.js
+++ queryProviders/demo.js
@@ -1,21 +1,28 @@
 'use strict';
 
+// Name for this QueryProvider. Only used internally.
 module.exports.name = 'demo';
 
+// List of metrics provided by a run of this query
+// These will appear in the autocomplete in the "Metrics" tab
 module.exports.targets = [
   'Target 1',
   'Target 2',
   'Target 3'
 ];
 
+// If the query resultset returns data for multiple targets,
+// this helps identify the target for every row
 module.exports.getTarget = (row) => {
   return row.target;
 };
 
+// Returns a datapoint [ metric-value, timestamp ] for a given row
 module.exports.convertToDatapoint = (row) => {
   return [ parseInt(row.cnt), (Date.UTC(row.year, row.month, 0)) ];
 };
 
+// Query to be executed
 module.exports.query = `
 (
 SELECT 'Target 1' as target, extract(year from dd) as year, extract(month from dd) as month, trunc(random() * 20 + 1) as cnt
@@ -57,6 +64,7 @@ LIMIT $1
 )
 `;
 
+// Pre-process input parameters for the query
 module.exports.run = (loadData, params, callback) => {
   let rStart = new Date(params.rangeStart);
   rStart.setHours(0, 0, 0, 0);
